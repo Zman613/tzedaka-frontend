a35b82e8f37901f172b830fe129deb7072c3ff38
zman613 <monthlyschnitzel@gmail.com>
2020-10-08 19:54:00 -0400

    fetching charities

--- Containers/AddCharity.js
+++ Containers/AddCharity.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
-const AddCharity = () => {
+const AddCharity = ({navigation}) => {
+
+  const [charities, setCharities] = useState([])
  
+  useEffect(() => {
+    fetch(`http://localhost:3000/charities`)
+    .then(resp => resp.json())
+    .then(charities => {
+      setCharities(charities)
+    })
+  }, [])
+
+  console.log(charities)
+
   return (
     <View>
       <Text>Hello</Text>
